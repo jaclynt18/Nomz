9c72011a2c372b6060755c530bb6cf4adf8ac1f5
“jaclyn <jaclyn.tsiang@tufts.edu>
2017-11-11 20:46:10 -0500

    putting html files together

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var path = require('path')
 var app = express()
 var bodyParser = require('body-parser')
 var index = require('./routes/index')
+var user = require('./routes/user')
+var donate = require('./routes/donate')
 
 
 app.set('views', path.join(__dirname, 'views'))
@@ -11,15 +13,14 @@ app.set('view engine', 'pug')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use('/', index)
 app.use(function(req, res, next) {
 	console.log(`$(req.method} request for '${req.url}' - ${JSON.stringify(req.body)}`);
 	next();
 });
 
-app.get('/login', function(req, res, next){
-	res.sendFile(path.join(__dirname, '/public/stylesheets', 'login.html'))
-})
+app.use('/', user)
+app.use('/', index)
+app.use('/donate_pg.html', donate)
 
 app.listen(process.env.PORT || 8000 , function() {
 	console.log("Listening on port 8000")
